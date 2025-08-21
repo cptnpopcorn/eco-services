d373ff9072c583a537325dd4cea741a586f64bf8
Hagen Hentschel <73716959+cptnpopcorn@users.noreply.github.com>
2025-08-21 23:58:08 +0200

    re-structure self-signed certificate scripts
    extract ssl commands into shell functions, to allow using them to create and sign other keys
    for example to generate authentication keys for MQTT sensors that need to authenticate against the broker on this server

--- components/self-signed-certificate/openssl/create-keys.sh
+++ components/self-signed-certificate/openssl/create-keys.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
-source $(dirname "$0")/check-vars.sh
 
-FILE_PRIV_CA="$DIR"/"ca/priv/ca"
-FILE_PUB_CA="$DIR"/"ca/pub/ca"
-FILE_PRIV_SRV="$DIR"/"srv/priv/srv"
-FILE_PUB_SRV="$DIR"/"srv/pub/srv"
+[ -z "${DIR}" ] && { echo "target key DIR undefined"; exit 1; }
+[ -d "${DIR}" ] || { echo "$DIR does not exist"; exit 1; }
+[ -z "${COUNTRY}" ] && { echo "COUNTRY undefined"; exit 1; }
+[ -z "${ORG}" ] && { echo "ORG (organization) undefined"; exit 1; }
+[ -z "${DOMAIN}" ] && { echo "DOMAIN name undefined"; exit 1; }
 
-[ -e "$FILE_PUB_CA.crt" ] && echo "CA certificate already present" || { \
+source $(dirname "$0")/ssl-functions.sh
+
+[ -e "${FILE_PUB_CA}.crt" ] && echo "CA certificate already present" || { \
 echo "creating new CA certificate"; \
-openssl req -days 36500 -newkey rsa:2048 -nodes -extensions v3_ca -keyout "$FILE_PRIV_CA.key" -subj "/C=$COUNTRY/O=$ORG/OU=Root CA/CN=$DOMAIN" -x509 -out "$FILE_PUB_CA.crt"; \
+create_ca_cert "${DOMAIN}"; \
 }
 
-[ -e "$FILE_PUB_SRV.csr" ] && echo "server signing request already present" || { \
-echo "creating server signing request"; \
-openssl req -newkey rsa:2048 -nodes -keyout "$FILE_PRIV_SRV.key" -subj "/C=$COUNTRY/O=$ORG/CN=$DOMAIN" -addext "subjectAltName=DNS:$DOMAIN" -addext "basicConstraints=critical,CA:FALSE" -out "$FILE_PUB_SRV.csr"; \
-}
+FILE_PRIV_SRV="${DIR}"/"srv/priv/srv"
+FILE_PUB_SRV="${DIR}"/"srv/pub/srv"
 
-[ -e "$FILE_PUB_SRV.crt" ] && echo "signed server certificate already present" || { \
-echo "signing server certificate"; \
-openssl x509 -req -CA "$FILE_PUB_CA.crt" -CAkey "$FILE_PRIV_CA.key" -days 36500 -CAcreateserial -copy_extensions copyall -in "$FILE_PUB_SRV.csr" -out "$FILE_PUB_SRV.crt"; \
+[ -e "${FILE_PUB_SRV}.crt" ] && echo "server sertificate already present" || { \
+echo "creating server private key and public certificate"; \
+create_and_sign_key "${DOMAIN}" "${FILE_PRIV_SRV}" "${FILE_PUB_SRV}"; \
 }
---
+++ components/self-signed-certificate/openssl/ssl-functions.sh
@@ -0,0 +1,19 @@
+# environment vars COUNTRY ORG DIR need to be set
+
+FILE_PRIV_CA="${DIR}"/"ca/priv/ca"
+FILE_PUB_CA="${DIR}"/"ca/pub/ca"
+
+# key file arguments are defined as file paths without an extension - .key / .csr / .crt being added automatically according to common conventions
+
+# 1 arg: CN
+create_ca_cert()
+{
+openssl req -days 36500 -newkey rsa:2048 -nodes -extensions v3_ca -keyout "${FILE_PRIV_CA}.key" -subj "/C=${COUNTRY}/O=${ORG}/OU=Root CA/CN=$1" -x509 -out "${FILE_PUB_CA}.crt"
+}
+
+# 2 args: domain private_key_file public_key_file
+create_and_sign_key()
+{
+openssl req -newkey rsa:2048 -nodes -keyout "$2.key" -subj "/C=${COUNTRY}/O=${ORG}/CN=$1" -addext "subjectAltName=DNS:$1" -addext "basicConstraints=critical,CA:FALSE" -out "$3.csr"
+openssl x509 -req -CA "${FILE_PUB_CA}.crt" -CAkey "${FILE_PRIV_CA}.key" -days 36500 -CAcreateserial -copy_extensions copyall -in "$3.csr" -out "$3.crt"
+}
