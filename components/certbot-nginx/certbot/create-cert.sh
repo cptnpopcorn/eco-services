923571d0ba21760e33d7b66a3c660bf7e45ebe2e
Hagen Hentschel <73716959+cptnpopcorn@users.noreply.github.com>
2026-03-04 21:54:46 +0100

    fix certbot usage bug
    was only ever applying the last set of options for multiple domains / subdomains .. this doesn't work
    has to be invoked once for every single (sub)domain

--- components/certbot-nginx/certbot/create-cert.sh
+++ components/certbot-nginx/certbot/create-cert.sh
@@ -1,3 +1,7 @@
-SUBDOMAINS=""
-for prefix in ${DOMAINPREFIXES:-}; do SUBDOMAINS="${SUBDOMAINS} --cert-name $prefix.$DOMAIN -d $prefix.$DOMAIN"; done
-certbot certonly --config /etc/certbot/cli.ini -d ${DOMAIN} ${SUBDOMAINS} && echo done > /msg/in
+certbot certonly --config /etc/certbot/cli.ini --cert-name ${DOMAIN} -d ${DOMAIN} || exit 1
+
+for prefix in ${DOMAINPREFIXES:-}; do
+	certbot certonly --config /etc/certbot/cli.ini --cert-name ${prefix}.${DOMAIN} -d ${prefix}.${DOMAIN} || exit 1
+done
+
+echo done > /msg/in
